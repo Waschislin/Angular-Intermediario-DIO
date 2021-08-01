3533cb44534c1e145080534beb890fcafc97bab0
Waschislin <wlalucena@gmail.com>
2021-08-01 20:59:49 -0300

    Reupando projeto por bug no arquivo anterior

---
+++ README.md
@@ -0,0 +1,2 @@
+# Angular-Intermediario-DIO
+Projeto do curso de angular intermediario ministrado pela plataforma Digital Innovation One
---
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { Filme } from 'src/app/shared/models/filme';
+
+@Component({
+  selector: 'dio-listagem-filmes',
+  templateUrl: './listagem-filmes.component.html',
+  styleUrls: ['./listagem-filmes.component.scss']
+})
+export class ListagemFilmesComponent implements OnInit {
+
+  filmes: Filme[];
+  constructor(private filmesService: FilmesService) { }
+
+
+  ngOnInit(): void {
+    this.filmesService.listar().subscribe((filmes: Filme[]) => this.filmes = filmes);
+  }
+  onScroll(): void {
+  }
+
+
+}
