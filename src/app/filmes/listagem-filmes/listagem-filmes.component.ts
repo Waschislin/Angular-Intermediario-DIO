3fa75267aca59693ca37e779d95d72c02bc2a2fa
Waschislin <wlalucena@gmail.com>
2021-08-02 19:51:10 -0300

    Adicionando filtro e adicionando ordenação por mais recente

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -11,12 +12,32 @@ export class ListagemFilmesComponent implements OnInit {
 
   readonly qtdPagina = 4;
   pagina = 0;
+  texto: string;
+  genero: string;
   filmes: Filme[] = [];
-
-  constructor(private filmesService: FilmesService) { }
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+  constructor(private filmesService: FilmesService, private fb: FormBuilder) { }
 
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+      this.texto = val;
+      this.resetarConsulta();
+    });
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.genero = val;
+      this.resetarConsulta();
+    });
+
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção Cientifica', 'Comédia', 'Drama'];
+
     this.listarFilmes();
   }
   onScroll(): void {
@@ -24,6 +45,13 @@ export class ListagemFilmesComponent implements OnInit {
   }
   private listarFilmes(): void {
     this.pagina++;
-    this.filmesService.listar(this.pagina, this.qtdPagina).subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+    this.filmesService.listar(this.pagina, this.qtdPagina, this.texto, this.genero).subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
   }
+
+  private resetarConsulta(): void {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
+  }
+
 }
